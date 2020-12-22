305a3350ea24e92985579a44f118ce69f1a90413
parbilla <parbilla@gmail.com>
2020-12-22 18:41:33 +0000

    tasks

---
+++ fadv.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop_f - doesn't do anything
+ * @stack: doble pointer from struct
+ * @line_number: the number of every line
+ * Return: always
+ */
+
+void nop_f(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
